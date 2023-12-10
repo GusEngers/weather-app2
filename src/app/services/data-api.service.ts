be63fec9a2578911a95ea9e97bb86b1a4fa0be6f
GusEngers <gustavoengers6441@gmail.com>
2023-12-10 20:25:34 -0300

    Add: condicionales en componente search

--- src/app/services/data-api.service.ts
+++ src/app/services/data-api.service.ts
@@ -1,14 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import {Observable } from "rxjs"
 @Injectable({
   providedIn: 'root',
 })
 export class DataApiService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  getLocalAddres(): string {
-    return 'Argentina';
-  }
+  // getLocalAddres(): Observable<string> {
+  //   return subscribeOn("");
+  // }
 
   getWeather(locale: string) {
     return;
